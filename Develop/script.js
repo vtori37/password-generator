4b8456f5d0727fa29299a14825b76dcf4fdd2dc8
Tori Acer <vrice360@gmail.com>
2022-02-25 19:59:14 -0600

    creating develop branch

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,8 @@
 // Assignment code here
-
-
+var password = document.getElementById("password");
+  var chars= "0123456789";
+  var passwordLength = 15;
+  var password = "";
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +17,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+/* Assignment code here
+function 
+
+// Get references to the #generate element //
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input //
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button//
+
+generateBtn.addEventListener("click", writePassword);
+*/
