e7cc23278dd4b7d4de15fd9f6c49d8072a1605b3
Tori Acer <vrice360@gmail.com>
2022-02-26 12:04:33 -0600

    added variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,76 @@
 // Assignment code here
 var password = document.getElementById("password");
-  var chars= "0123456789";
-  var passwordLength = 15;
+
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()";
+
+// var passwordLength = function promptPasswordLength() {
+//   return givenLength;
+// }
+
+var givenLength = prompt("Please enter your desired password length between 8 and 128.", 8);
+
+var lowercaseChoice = confirm("Do you want to use lower case?");
+
+function generatePassword() {
   var password = "";
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  var pool = "";
+  if ( lowercaseChoice ) {
+    pool += lowercase
+  } 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  for (var i = 0; i < givenLength; i++) {
+    console.log("Im inside the for loop haha");
+    // from here, randomly select a character from the pool and concat it to the password
+    }
+  return password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// document.getElementById("password").value = password;
+
+
+// // Get references to the #generate element
+// var x = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   // var password = generatePassword();
+//   // var passwordText = document.querySelector("#password");
+//   console.log(x);
+//   // passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// x.addEventListener("click", writePassword);
 
 
 
-/* Assignment code here
-function 
+// //Assignment code here
+// function 
 
 // Get references to the #generate element //
 var generateBtn = document.querySelector("#generate");
 
+// function generatePassword() {
+//   var x = "Password1.0";
+//   return x;
+// }
+
 // Write password to the #password input //
 function writePassword() {
+  
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var x = document.querySelector("#password");
 
-  passwordText.value = password;
+  x.value = password;
 
 }
 
 // Add event listener to generate button//
 
 generateBtn.addEventListener("click", writePassword);
-*/
+
